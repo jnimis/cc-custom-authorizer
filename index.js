3aae67a5eae2a424ebbcf36fdd187fff7692bea4
John Nimis <john.nimis@gmail.com>
2024-08-29 18:31:36 -0500

    working auth0 authorizer

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var AWS = require('aws-sdk');
 const lib = require('./lib');
 let data;
 
+const AccessCodes = Object.freeze({
+  UNKNOWN_USER:  0,
+  ALLOW:         1,
+  UNAUTHORIZED:  2,
+  SYSTEM_ERROR:  11
+});
+
 const get_admin_api_token = async () => {
   var ssm = new AWS.SSM({region: 'us-east-1'});
   console.log("initialized ssm");
@@ -39,17 +46,39 @@ const is_authorized = async (user_id, gym_id) => {
     redirect: 'follow'
   };
 
-  await fetch("https://onemanband.auth0.com/api/v2/users?q=user_id%3A" + encoded_user_id, requestOptions)
+  let err = await fetch("https://onemanband.auth0.com/api/v2/users?q=user_id%3A" + encoded_user_id, requestOptions)
     .then(response => response.text())
     .then(result => {
-      console.log("fetch user result: " + result);
-      if (result.statusCode == 200) {
-        // get gym_id and check against actual id
+      console.log(result);
+      let resultObj = JSON.parse(result);
+      if (resultObj.statusCode == 401) {
+        return "exception: invalid auth0 api key";
+      }
+      let metadataObj = JSON.parse(result)[0].app_metadata;
+      if (!metadataObj) {
+        return "no metadata for user " + user_id;
+      }
+      let access_level = metadataObj["gym_" + gym_id];
+      if (!access_level) {
+        return "user has not tried to register at this gym";
+      }
+      if (access_level < 1) {
+        return "user has gym record but is not authorized";
       } else {
-        return false;
+        return "";
       }
     })
-    .catch(error => console.log("fetch user error", error));
+    .catch(error => {
+      return "exception: " + error;
+    });
+    console.log(err);
+    if (err == "") {
+      return ["authorized", AccessCodes.ALLOW];
+    } else if (err.startsWith("exception")) {
+      return ["system error", AccessCodes.SYSTEM_ERROR];
+    } else {
+      return [err, AccessCodes.UNAUTHORIZED];
+    }
 }
 
 module.exports.handler = async (event, context, callback) => {
@@ -57,19 +86,23 @@ module.exports.handler = async (event, context, callback) => {
     console.log("start of function");
 
     data = await lib.authenticate(event);
-    console.log("authenticate complete");
+    console.log("authentication complete");
     
     // get metadata from auth0
     let user_id = data.principalId;
     console.log("user id is " + user_id);
-    if (await is_authorized(user_id, 1)) {
+    const [message, access_level] = await is_authorized(user_id, 1); // "google-oauth2|106647354996701306231"
+    console.log(message);
+    if (access_level == AccessCodes.ALLOW) {
       return data;
+    } else if (access_level == AccessCodes.SYSTEM_ERROR) {
+      return context.fail("There was a system error: 2");
     } else {
       return context.fail("User not authorized for this gym");
     }
   }
   catch (err) {
       console.log(err);
-      return context.fail("Unauthorized");
+      return context.fail("There was a system error: 3");
   }
 };
